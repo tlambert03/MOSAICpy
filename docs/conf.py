9f9888a0206862defd20530784b70b80a8d294db
Talley Lambert <talley.lambert@gmail.com>
2019-10-03 11:20:03 -0400

    add mocks to docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,10 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-autodoc_mock_imports = ["numpy"]
+autodoc_mock_imports = [
+    "numpy", "tifffolder", "tifffile", "PyQt5", "sentry-sdk", "numba", "matplotlib",
+    "scipy"
+]
 
 # -- Project information -----------------------------------------------------
 
